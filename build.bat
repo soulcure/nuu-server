ade30a373389dc474a56d7e8cf19c1edb531260a
colin <287268221@qq.com>
2019-07-05 11:47:58 +0800

    nuu server update

--- build.bat
+++ build.bat
@@ -1,12 +1,12 @@
 @echo off
 
-if exist build-linux.bat goto ok
+if exist build.bat goto ok
 echo install.bat must be run from its folder
 goto end
 
 : ok
 
-SET dist=go_linux_amd64
+SET dist=go_win_amd64
 
 SET GOPATH=%~dp0;%GOPATH%
 
--- src/main/main.go
+++ src/main/main.go
@@ -50,8 +50,12 @@ func main() {
 
 	config := iris.WithConfiguration(iris.YAML("./conf/iris.yml"))
 
-	if err := app.Run(iris.Addr(":8899"), config); err != nil {
+	if err := app.Run(iris.Addr("119.23.74.49:8899"), config); err != nil {
 		logrus.Error(err)
 	}
+
+	/*if err := app.Run(iris.Addr(":8899"), config); err != nil {
+		logrus.Error(err)
+	}*/
 
 }
